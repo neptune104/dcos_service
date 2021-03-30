77cbda6a2344c985cd0eaadfa7f318c98b8061cd
neptune104 <35022983+neptune104@users.noreply.github.com>
2021-03-30 18:20:34 +0900

    Update dcos-master-check.sh

--- dcos-master-check.sh
+++ dcos-master-check.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-for i in $(ls /etc/systemd/system/dcos.target.wants/ -1 |grep -Ev "socket" |grep -Ev "timer"); do echo $i; systemctl status $i |grep Active ; done;
+echo "|              Service name             |  Status  | Msg |"
+echo "|---------------------------------------|----------|-----|"
+for i in $(ls /etc/systemd/system/dcos.target.wants/ -1 |grep -Ev "socket" |grep -Ev "timer"); do
+        systemctl is-active --quiet $i && result="Active" || result="Inactive";
+
+        if [ $result == "Active" ]; then
+                echo -e "|  $i  | \e[1;32;40m  $result \e[0m |"
+        else
+                systemctl is-failed --quiet $i && result="Failed" || result="Inactive";
+                #echo -e "|  $i  | $result |"
+                if [ $result == "Inactive" ]; then
+                        echo -e "|  $i  | $result |"
+                else
+                        echo -e "|  $i  | $result |"
+                fi
+        fi
+
+done;
